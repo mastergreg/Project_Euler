a5b65d506fc9dca5a463f887838dd9386475be21
Gregory Liras <gregliras@gmail.com>
2012-01-18 17:17:03 +0200

    got input

--- pr_82/main.py
+++ pr_82/main.py
@@ -7,7 +7,7 @@
 #
 #* Creation Date : 18-01-2012
 #
-#* Last Modified : Wed 18 Jan 2012 02:42:15 AM EET
+#* Last Modified : Wed 18 Jan 2012 05:16:25 PM EET
 #
 #* Created By : Greg Liras <gregliras@gmail.com>
 #
@@ -17,6 +17,16 @@ import sys
 
 def main():
     f = sys.stdin
+    length = int(f.readline())
+    ls=[]
+    for i in range(length):
+        ls.append(map(lambda x : int(x),f.readline().split()))
+    ls = zip(*ls[::-1])
+    ls = map(list,ls)
+
+    dyntable = list(ls)
+
+
 
 
 
