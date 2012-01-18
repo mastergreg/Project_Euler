2cdf315d775a951b05521a493e7114c73e7beb2a
Gregory Liras <gregliras@gmail.com>
2012-01-18 03:39:24 +0200

    starting 82

---
+++ pr_82/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+#
+#* File Name : main.py
+#
+#* Purpose :
+#
+#* Creation Date : 18-01-2012
+#
+#* Last Modified : Wed 18 Jan 2012 02:42:15 AM EET
+#
+#* Created By : Greg Liras <gregliras@gmail.com>
+#
+#_._._._._._._._._._._._._._._._._._._._._.*/
+
+import sys
+
+def main():
+    f = sys.stdin
+
+
+
+if __name__=="__main__":
+    main()
+
