3f5d2d5a7f469008f31f2f956be0c3186bb4fc31
Gregory Liras <gregliras@gmail.com>
2012-02-29 14:51:38 +0200

    73 complete :

--- pr_073/solve.py
+++ pr_073/solve.py
@@ -7,7 +7,7 @@
 #
 #* Creation Date : 09-02-2012
 #
-#* Last Modified : Fri 10 Feb 2012 03:46:00 PM EET
+#* Last Modified : Wed 29 Feb 2012 02:48:03 PM EET
 #
 #* Created By : Greg Liras <gregliras@gmail.com>
 #
@@ -16,17 +16,16 @@ import psyco
 
 psyco.full()
 
-from fractions import Fraction
+from fractions import Fraction,gcd
 def main():
-    ans = Fraction(1,4)
-    lim = Fraction(3,7)
-    for j in range(1000001,1,-1):
-        print ans
-        for i in range(int(ans*j)-1,j/2+1):
-            k = Fraction( i, j)
-            if k < lim and k > ans:
-                ans = k
-    print ans
+    ans = set()
+    for j in range(1,12001):
+        print j
+        for i in range(j/3-3,j/2+2):
+            if i*3 > j and i*2 < j:
+                g = gcd(i,j)
+                ans.add((i/g,j/g))
+    print len(list(ans))
 
 
 
