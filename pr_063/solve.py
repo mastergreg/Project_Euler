844b1866589af1ee9e0a48b800bc64c5837ecdb4
Gregory Liras <gregliras@gmail.com>
2012-03-09 11:24:36 +0200

    solved 63

--- pr_063/solve.py
+++ pr_063/solve.py
@@ -7,7 +7,7 @@
 #
 #* Creation Date : 14-10-2011
 #
-#* Last Modified : Mon 06 Feb 2012 12:32:37 PM EET
+#* Last Modified : Fri 09 Mar 2012 11:24:24 AM EET
 #
 #* Created By : Greg Liras <gregliras@gmail.com>
 #
@@ -17,32 +17,19 @@
 from math import *
 
 
-def nth_power_n_digits(i):
-  if log10(pow(2,i))+1>i :
-    return False
-  t = 1.0
-  j = 1
-  for j in range(1,10):
-    t = pow(j,i)
-    if (log10(t) + 1) >= i:
-      return True
-
-def has_power(num):
-    i=1
-    while(int(ceil(log10(i**num)))<num and i<=10 ):
-        i=i+1
-
-    if (int(ceil(log10(i**num))) == num ):
-        return True
-    else:
-        return False
+def calc_nth_ndig(num):
+    k=0
+    for i in xrange(1,10):
+        x = int(ceil(log10(i**num)))
+        if  x == num:
+            k+=1
+    return k
 
 
 def main():
-    k=0
-    for i in range(10000):
-        if(has_power(i)):
-            k=k+1
+    k=1
+    for i in range(1,1000):
+        k+=calc_nth_ndig(i)
     print k
 
 
