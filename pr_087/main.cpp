b400c220ffa268906bcd4102da6660187bd88705
Gregory Lyras <gregliras@gmail.com>
2014-06-06 19:28:28 +0100

    87 complete

--- pr_087/main.cpp
+++ pr_087/main.cpp
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name : main.cpp
 * Creation Date : 10-05-2014
-* Last Modified : Fri 16 May 2014 08:08:52 PM BST
+* Last Modified : Fri 06 Jun 2014 19:27:13 BST
 * Created By : Greg Lyras <greglyras@gmail.com>
 _._._._._._._._._._._._._._._._._._._._._.*/
 #include <iostream>
@@ -11,15 +11,21 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #include <set>
 #include <iterator>
 #include <cmath>
-#include <cassert>
+#include <cstring>
 
-#define MAX_PRIME 50000000
-#define FAMILY_TARGET 7
+#include "test.h"
+
+#define MAX_PRIME 10000
+#define MAX_CEIL 50000000
+#define SND_CEIL 7072
+#define THRD_CEIL 869
+#define FRTH_CEIL 85
 
 using namespace std;
 
 list<int> primes;
 map<int, bool> primes_check;
+map<int, bool> checked;
 
 map<int, map<int, list<int> > > masks;
 
@@ -29,30 +35,59 @@ bool is_prime(int i)
     return primes_check[i];
   }
   else {
-    for(int p : primes) {
+    for(auto p : primes) {
       if(i % p == 0) {
         return false;
       }
       else if(p >= sqrt(i)) {
+        primes_check[i] = true;
         return true;
       }
     }
+    primes_check[i] = true;
     return true;
   }
 }
 
 
-int main(void)
+
+int main(int argc, char **argv)
 {
   primes.push_back(2);
   primes_check[2] = true;
   for(int i = 3; i < MAX_PRIME; i+=2) {
     if(is_prime(i)) {
       primes.push_back(i);
-      primes_check[i] = true;
     }
   }
+
+  if(argc == 2 and strcmp(argv[1], "test") == 0) {
+    run_tests();
+    exit(EXIT_SUCCESS);
+  }
+
   cout << primes.size() << endl;
+  int count = 0;
+
+  for(auto x : primes) {
+    if(x <= SND_CEIL) {
+      for(auto y : primes) {
+        if(y <= THRD_CEIL) {
+          for(auto z : primes) {
+            if(z <= FRTH_CEIL) {
+              uint64_t sum = x*x + y*y*y + z*z*z*z;
+              if(sum > 0 and sum < MAX_CEIL and checked[sum] == false) {
+                checked[sum] = true;
+                count++;
+                cout << x << " " << y << " " << z << " " << sum << endl;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  cout << count << endl;
 
   return 0;
 }
---
+++ pr_087/test.h
@@ -0,0 +1,15 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : test.h
+* Creation Date : 06-06-2014
+* Last Modified : Fri 06 Jun 2014 19:05:05 BST
+* Created By : Greg Lyras <greglyras@gmail.com>
+_._._._._._._._._._._._._._._._._._._._._.*/
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#include <cassert>
+
+bool is_prime(int i);
+void run_tests(void);
+
+#endif /* __TEST_H__ */
